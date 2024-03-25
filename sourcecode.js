83ba644122f2496233769fcfd43668af45029885
AndrewM <andrew@professionalarts.com>
2024-03-25 11:26:03 -0500

    move row-container and square generation to js file

--- sourcecode.js
+++ sourcecode.js
@@ -1,2 +1,27 @@
-const gridContiner = document.querySelector("#grid-container");
+const gridContainer = document.querySelector("#grid-container");
 
+function populateGrid(gridSize) {
+    console.log("This launched!");
+    const gridSizeInt = parseInt(gridSize);
+    console.log(gridSizeInt);
+
+    for (let i = 0; i < gridSizeInt; i++) {
+        gridContainer.appendChild(createRowContainer(gridSizeInt));
+    }
+}
+
+function createRowContainer(gridSize) {
+    console.log("We got here too!");
+    const rowContainer = document.createElement("div");
+    rowContainer.classList.toggle("row-container");
+
+    for (let i = 0; i < gridSize; i++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square");
+        rowContainer.appendChild(square);
+    }
+
+    return rowContainer;
+}
+
+populateGrid("30");
